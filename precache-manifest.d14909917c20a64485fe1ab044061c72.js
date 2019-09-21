1ed0d93b1f28aec08c0e2ecc7240298798266c1e
kianga722 <kianga722@gmail.com>
2019-09-20 20:39:44 -0400

    Updates

---
+++ precache-manifest.d14909917c20a64485fe1ab044061c72.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2590fb4a257c0f40daab3edc344da507",
+    "url": "/bike-rental/index.html"
+  },
+  {
+    "revision": "da1dcc7858e7aec2bdba",
+    "url": "/bike-rental/static/css/main.2d1c276c.chunk.css"
+  },
+  {
+    "revision": "8582c93a4514c85b9da6",
+    "url": "/bike-rental/static/js/2.b29354d1.chunk.js"
+  },
+  {
+    "revision": "da1dcc7858e7aec2bdba",
+    "url": "/bike-rental/static/js/main.60a24c07.chunk.js"
+  },
+  {
+    "revision": "c912bb56f75cca095124",
+    "url": "/bike-rental/static/js/runtime~main.a19c5562.js"
+  }
+]);
